8e6a5de7c6a3eae94a7a5c2df91b37236ec9dd1d
Hank Beaver <hbeaver@gmail.com>
2015-03-31 16:36:16 -0400

    [#15, #13] Added more CREATE and UPDATE specs

--- spec/resources_api/update_spec.rb
+++ spec/resources_api/update_spec.rb
@@ -10,15 +10,20 @@ RSpec.describe "update" do
 
   let(:existing_resource) { Post.make!(body: 'not nice body') } 
   
-  let(:post_body) { 'a nice body2' }
-  let(:create_params) do
+  let(:post_body) { 'a nice body' }
+  let(:post_title)    { 'even nicer title' }
+
+  
+  let(:valid_params) do
     { 
       "#{resource_name}" =>
       {
-        body: post_body
+        body: post_body,
+        title: post_title
       }
     }
   end
+  let(:create_params) { valid_params } 
 
   with_request "PATCH /#{collection_name}/:id.json"  do
     before do 
@@ -33,12 +38,36 @@ RSpec.describe "update" do
     it_expects(:resource_body)   { expect(body[top_level_key].execute).to be_a(Hash) }
 
     it "should update resource" do
+      perform_request!
+      it_should_respond_with_resource(resource_class.first)
+      it_expects_resource_key_to_match(:body, post_body)
+    end
+    it "should not create another resource" do
+      perform_request!
+      posts = Post.where(title: post_title).to_a
+      expect(posts.size).to eq(1)
+    end
+  end
 
+  with_request "PUT /#{collection_name}/:id.json"  do
+    before do 
+      request_params.merge!(create_params)
+      request_path "/#{collection_name}/#{existing_resource.id}.json"
+    end
+    its_status_should_be 200
+    it_should_have_content_length
+
+    it_expects(:content_type)    { expect(headers["Content-Type"]).to include("application/json") }
+    it_expects(:utf8)            { expect(headers["Content-Type"]).to include("charset=utf-8") }
+    it_expects(:resource_body)   { expect(body[top_level_key].execute).to be_a(Hash) }
+
+    it "should update resource" do
       perform_request!
       it_should_respond_with_resource(resource_class.first)
       it_expects_resource_key_to_match(:body, post_body)
     end
 
   end
+
 end
 
