4bcccc4247941006dda353f3afd9db981bf73be2
Hank Beaver <hbeaver@gmail.com>
2015-04-07 12:38:27 -0400

    [#20] Add matterhorn_url_options for :self on singleton routes

--- spec/internal/app/models/topic.rb
+++ spec/internal/app/models/topic.rb
@@ -8,4 +8,10 @@ class Topic
 
   validates_presence_of :name
 
+  # Use this method to override the url_for on this nested,singleton resource. 
+  # The below return will be handed as-is to ActionDispatch::Routing::UrlFor
+  def matterhorn_url_options
+    [post, 'topic']
+  end
+
 end
